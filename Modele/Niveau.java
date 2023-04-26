371dfc78f9f7253bb9e476ca3b1de521237b87af
Benfa7 <benjaminfa7@gmail.com>
2023-04-26 09:59:44 +0200

    sans_accent

--- Modele/Niveau.java
+++ Modele/Niveau.java
@@ -1,4 +1,4 @@
-package Modèle;
+package Modele;
 
 public class Niveau {
     
---
+++ Tablut.java
@@ -0,0 +1,16 @@
+
+import Controlleur.ControlleurMediateur;
+import Modele.Jeu;
+
+
+import java.security.InvalidParameterException;
+import java.util.Iterator;
+
+public class Tablut{
+    public static void main(String[] args) {
+        Jeu j = new Jeu();
+        //CollecteurEvenements control = new ControlleurMediateur(j);
+        //InterfaceGraphique.demarrer(j, control);
+    }
+}
+ 
