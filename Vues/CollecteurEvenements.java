3fc1df89c0ea3edf6d0edf1a99a6f7fa4dbba382
Benfa7 <benjaminfa7@gmail.com>
2023-04-26 10:03:52 +0200

    controlleur

---
+++ Vues/CollecteurEvenements.java
@@ -0,0 +1,9 @@
+package Vues;
+
+public class CollecteurEvenements {
+    // void clicSouris(int l, int c);
+    // void tictac();
+    // void changeJoueur(int j, int t);
+    // void changeTaille(int t);
+}
+
