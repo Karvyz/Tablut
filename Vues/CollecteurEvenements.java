96d09b6733098e528bd60c09048d0cb47466f1f8
FAVRE <benjaminfa7@gmail.com>
2023-04-28 16:51:25 +0200

    Rebase IHM V3

--- Vues/CollecteurEvenements.java
+++ Vues/CollecteurEvenements.java
@@ -1,6 +1,6 @@
 package Vues;
 
-import Modele.*;
+import Modele.Coordonne;
 
 public interface CollecteurEvenements {
 
@@ -9,37 +9,6 @@ public interface CollecteurEvenements {
     // void changeTaille(int t);
 
     void clicSouris(int l, int c);
-
-    void fixerMediateurVues(Vues v);
-
-    void afficherDemarrage();
-
-    void afficherMenuPrincipal();
-
-    void afficherMenuNouvellePartie();
-
-    void afficherJeu();
-
-    void afficherMenuChargerPartie();
-
-    void nouvellePartie(String nomJ1, TypeJoueur typeJ1, String nomJ2, TypeJoueur typeJ2);
-
-    void partieSuivante();
-
-    Jeu jeu();
-
-    void toClose();
-
-    void afficherRegles();
-
-    void annuler();
-
-    void refaire();
-
-    void toucheClavier(String touche);
-
-    boolean sauvegarderPartie();
-
-    void chargerPartie(String nomSauvegarde);
+    void dragANDdrop(Coordonne coordonne, Coordonne coordonne2);
 }
 
