7b11b6f2dfca37f8ea2700ec4681d96f0a6efdd2
FAVRE <benjaminfa7@gmail.com>
2023-04-30 14:28:57 +0200

    Mise en bleu du pion en cours de déplacement. Bug en (0,0) lors de passage sur la coordonne en (5,5)

--- Vues/AdaptateurSouris.java
+++ Vues/AdaptateurSouris.java
@@ -27,14 +27,16 @@ public class AdaptateurSouris extends MouseAdapter implements MouseMotionListene
     @Override
     public void mousePressed(MouseEvent e) {
         dragStart = e.getPoint();
-        int l = e.getY() / plateau.hauteurCase;
-        int c = e.getX() / plateau.largeurCase;
+        int l = (int) Math.floor((double) e.getY() / plateau.hauteurCase);
+        int c = (int) Math.floor((double) e.getX() / plateau.largeurCase);
         Pion caseSelec = plateau.jeu.n.getPion(l,c);
         if (!plateau.jeu.n.check_clic_selection_pion(caseSelec, plateau.jeu.joueurCourant())){
             plateau.setPionEnDeplacement(null);
         }
         else{
-            plateau.setPionEnDeplacement(new Point(c * plateau.largeurCase + plateau.largeurCase / 2, l * plateau.hauteurCase + plateau.hauteurCase / 2));            plateau.setCouleurEnDeplacement(l, c); // Ajoutez cette ligne pour définir la couleur du pion en déplacement
+            plateau.setPionSelec(new Point(c * plateau.largeurCase + plateau.largeurCase / 2, l * plateau.hauteurCase + plateau.hauteurCase / 2)); //permet de garder en mémoire le pion selec
+            plateau.setPionEnDeplacement(new Point(c * plateau.largeurCase + plateau.largeurCase / 2, l * plateau.hauteurCase + plateau.hauteurCase / 2));            
+            plateau.setCouleurEnDeplacement(l, c); // Ajoutez cette ligne pour définir la couleur du pion en déplacement
         }
         System.out.println("Clic en (" + l + "," + c + ")");
         control.clicSouris(l, c);
@@ -49,6 +51,10 @@ public class AdaptateurSouris extends MouseAdapter implements MouseMotionListene
             int mouseY = e.getY();
             pionEnDeplacement.setLocation(mouseX, mouseY); //modifie les coordonne du Point pionEnDeplacement
             plateau.setPionEnDeplacement(pionEnDeplacement);
+
+            // int l = mouseY / plateau.hauteurCase;
+            // int c = mouseX / plateau.largeurCase;
+            // System.out.println(l + "," + c );
         }
     }
 
@@ -59,14 +65,15 @@ public class AdaptateurSouris extends MouseAdapter implements MouseMotionListene
         if (dragStart != null) {
             int startX = dragStart.x / plateau.largeurCase;
             int startY = dragStart.y / plateau.hauteurCase;
-            int endX = e.getX() / plateau.largeurCase;
-            int endY = e.getY() / plateau.hauteurCase;
+            int endX = (int) Math.floor((double) e.getX() / plateau.largeurCase);
+            int endY = (int) Math.floor((double) e.getY() / plateau.hauteurCase);
 
             if (startX != endX || startY != endY) {
                 control.dragANDdrop(new Coordonne(startY, startX), new Coordonne(endY, endX));
             }
         }
         plateau.setPionEnDeplacement(null);
+        plateau.setPionSelec(null);
     }
 
 }
