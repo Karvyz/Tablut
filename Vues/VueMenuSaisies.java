ff3540aad9496015f8597a6293f9d1cff3796355
tom7438 <tom192010@live.fr>
2023-05-02 23:30:24 +0200

    Modif affichage menuSaisies + pièces plus grandes (assets)

--- Vues/VueMenuSaisies.java
+++ Vues/VueMenuSaisies.java
@@ -14,50 +14,51 @@ import java.lang.reflect.Type;
 public class VueMenuSaisies extends JPanel {
 
     CollecteurEvenements controleur;
-
     private JPanel MenuSaisies;
     private JTextField nomJ1;
     private JComboBox typeJ1;
     private JButton jouerButton;
     private JButton menuPrincipalButton;
+    private JButton quitter;
     private JPanel Boutons;
+    private JPanel BoutonsQuitter;
     private JPanel Joueur2;
     private JPanel Joueur1;
     private JTextField nomJ2;
     private JComboBox typeJ2;
-    private JLabel JoueurCommence;
     private JComboBox comboBox1;
     int logoHeight;
     Image background;
-    Image vs;
+    Image title;
     int called = 0;
 
     public VueMenuSaisies(CollecteurEvenements c) {
         // Chargement des assets
         background = Imager.getImageBuffer("assets/logo.png");
+        title = Imager.getImageBuffer("tablut.png");
 
         controleur = c;
 
         typeJ1 = new CComboxBox();
         typeJ2 = new CComboxBox();
-        menuPrincipalButton = new CButton();
-        jouerButton = new CButton().blanc();
+        menuPrincipalButton = new CButton("Menu Principal");
+        jouerButton = new CButton("Jouer").blanc();
         comboBox1 = new CComboxBox();
         nomJ1 = new CTextField();
         nomJ2 = new CTextField();
         Boutons = new JPanel();
+        Boutons.setOpaque(false);
+        BoutonsQuitter = new JPanel();
+        BoutonsQuitter.setOpaque(false);
 
         setBackground(Color.PINK);
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         setBorder(BorderFactory.createEmptyBorder(50, 0, 0, 0));
 
-        // Chargement des assets
-        //t = Imager.getImageBuffer("topbanner.png");
-        vs = Imager.getImageBuffer("assets/VS.png");
-
         MenuSaisies = this;
         setupUI();
 
+        // Modifie la couleur du fond et la police
         for (Component co : MenuSaisies.getComponents()) {
             if (co instanceof JPanel) {
                 for (Component coo : ((JPanel) co).getComponents()) {
@@ -65,10 +66,12 @@ public class VueMenuSaisies extends JPanel {
                     else if (coo instanceof JLabel) {
                         coo.setFont(new Font("Arial", Font.PLAIN, 14));
                         coo.setForeground(Color.WHITE);
+                        ((JLabel) coo).setHorizontalAlignment(SwingConstants.CENTER);
+                        //((JLabel) coo).setOpaque(false);
+                        //((JLabel) coo).setIcon(new ImageIcon("Mur.png"));
                     }
                 }
             }
-
         }
 
         menuPrincipalButton.addActionListener((e) -> controleur.afficherMenuPrincipal());
@@ -167,27 +170,29 @@ public class VueMenuSaisies extends JPanel {
         createUIComponents();
         MenuSaisies.setLayout(new GridLayout(4, 1));
         MenuSaisies.setMaximumSize(new Dimension(441, 110));
-        MenuSaisies.setMinimumSize(new Dimension(441, 110));
-        MenuSaisies.setPreferredSize(new Dimension(441, 110));
+        //MenuSaisies.setMinimumSize(new Dimension(441, 110));
+        MenuSaisies.setPreferredSize(new Dimension(441, 50));
 
         Joueur1 = new JPanel();
-        Joueur1.setLayout(new GridLayout(1, 4));
+        Joueur1.setLayout(new GridLayout(1, 3));
         Joueur1.setBackground(new Color(-14276823));
         Joueur1.setOpaque(false);
 
-        Joueur1.setMaximumSize(new Dimension(300, 110/3));
+        Joueur1.setMaximumSize(new Dimension(300, 20));
 
+        // Ajouter un bord à gauche
         MenuSaisies.add(Joueur1);
 
-        JLabel label1 = new JLabel(" Joueur 1");
+        JLabel label1 = new JLabel(" Attaquant");
         Joueur1.add(label1);
 
         nomJ1.setDropMode(DropMode.USE_SELECTION);
         nomJ1.setFocusCycleRoot(false);
         nomJ1.setFocusTraversalPolicyProvider(false);
         nomJ1.setFocusable(true);
-        nomJ1.setText("J1");
+        nomJ1.setText("Attaquant");
         nomJ1.setVisible(true);
+        //nomJ1.setOpaque(false);
         Joueur1.add(nomJ1);
 
         DefaultComboBoxModel<String> defaultComboBoxModel1 = new DefaultComboBoxModel<>();
@@ -199,7 +204,7 @@ public class VueMenuSaisies extends JPanel {
         Joueur1.add(typeJ1);
 
         Joueur2 = new JPanel();
-        Joueur2.setLayout(new GridLayout(1, 4));
+        Joueur2.setLayout(new GridLayout(1, 3));
         Joueur2.setBackground(new Color(-14276823));
         Joueur2.setOpaque(false);
 
@@ -207,14 +212,14 @@ public class VueMenuSaisies extends JPanel {
 
         MenuSaisies.add(Joueur2);
 
-        JLabel label2 = new JLabel(" Joueur 2");
+        JLabel label2 = new JLabel(" Défenseur");
         Joueur2.add(label2);
 
         nomJ2.setDropMode(DropMode.USE_SELECTION);
         nomJ2.setFocusCycleRoot(false);
         nomJ2.setFocusTraversalPolicyProvider(false);
         nomJ2.setFocusable(true);
-        nomJ2.setText("J2");
+        nomJ2.setText("Défenseur");
         nomJ2.setVisible(true);
         Joueur2.add(nomJ2);
 
@@ -229,9 +234,22 @@ public class VueMenuSaisies extends JPanel {
         Boutons.add(jouerButton);
         Boutons.add(menuPrincipalButton);
 
-        Boutons.setMaximumSize(new Dimension(441, 110/3));
+        Boutons.setMaximumSize(new Dimension(441, 30));
 
         MenuSaisies.add(Boutons);
+
+        // Quitter
+        quitter = new CButton("Quitter");
+        quitter.addActionListener((e) -> {
+            controleur.toClose();
+        });
+
+        BoutonsQuitter.add(quitter);
+        BoutonsQuitter.setMaximumSize(new Dimension(441, 30));
+
+
+        MenuSaisies.add(BoutonsQuitter);
+
     }
     @Override
     protected void paintComponent(Graphics g) {
@@ -240,23 +258,12 @@ public class VueMenuSaisies extends JPanel {
         // Affichage de l'image d'arrière plan
         g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
 
-
-        int w = getWidth(), h = getHeight();
-
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-        //Color color1 = new Color(255, 140, 85);
-        //Color color2 = new Color(255, 120, 105);
-        //GradientPaint gp = new GradientPaint(0, 0, color1, w, h, color2);
-        //g2d.setPaint(gp);
-        g2d.fillRect(0, 0, w, h);
-
         int width = (int) (getWidth() * 0.25);
-        logoHeight = (vs.getHeight(null) * width) / vs.getWidth(null);
+        logoHeight = (title.getHeight(null) * width) / title.getWidth(null);
 
-        int x = (int) ((getWidth() - width) / 2) - 5;
+        int x = ((getWidth() - width) / 2) - 5;
 
-        g.drawImage(vs, x, 35, width, logoHeight, null);
+        g.drawImage(title, x, 35, width, logoHeight, null);
         if (called == 0) {
             setBorder(BorderFactory.createEmptyBorder(logoHeight + 50, 0, 0, 0));
             called = 1;
