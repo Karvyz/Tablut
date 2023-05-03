0df12b7be68e97babc5696ec9fdf6fcde2d74a4a
4ppolyon <romalves70@gmail.com>
2023-05-03 09:30:18 +0200

    je sais pas ou est a bouclé ????

--- Controlleur/ControlleurMediateur.java
+++ Controlleur/ControlleurMediateur.java
@@ -1,21 +1,9 @@
 package Controlleur;
-import java.awt.Color;
-
 import Modele.Coordonne;
-import Modele.Data_Niveau;
 import Modele.Jeu;
 import Modele.Niveau;
 import Modele.Pion;
-import Structures.Pile;
 import Vues.CollecteurEvenements;
-import java.io.EOFException;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InvalidClassException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 
 
 public class ControlleurMediateur implements CollecteurEvenements {
--- Modele/Niveau.java
+++ Modele/Niveau.java
@@ -2,18 +2,12 @@ package Modele;
 import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InvalidClassException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Hashtable;
-
-import javax.print.DocFlavor.STRING;
-
-import Structures.Pile;
 
 
 public class Niveau implements Serializable, Cloneable {
