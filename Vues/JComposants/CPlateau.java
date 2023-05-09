945096b218999de31a2a2dfbab3eb83c3de3503c
tom7438 <tom192010@live.fr>
2023-05-09 22:48:41 +0200

    Ajustement taille plateau et dessin des images

--- Vues/JComposants/CPlateau.java
+++ Vues/JComposants/CPlateau.java
@@ -107,11 +107,11 @@ public class CPlateau extends JPanel implements Observateur {
                 //System.out.println("x = " + x + ", y = " + y);
 
                 if (n.estAttaquant(l, c)) {
-                    g.drawImage(Theme.instance().noir_inactif(), x+5, y+4, largeurCase-8, hauteurCase-8, this);
+                    g.drawImage(Theme.instance().noir_inactif(), x+4, y+4, largeurCase-8, hauteurCase-8, this);
                 } else if (n.estRoi(l, c)) {
                     g.drawImage(Theme.instance().roi(), x+5, y+5, largeurCase-7, hauteurCase-2, this);
                 } else if (n.estDefenseur(l, c)) {
-                    g.drawImage(Theme.instance().blanc_inactif(), x+5, y+4, largeurCase-8, hauteurCase-8, this);
+                    g.drawImage(Theme.instance().blanc_inactif(), x+4, y+4, largeurCase-8, hauteurCase-8, this);
                 }
                 x += largeurCase;
                 if(c%2 == 0)
