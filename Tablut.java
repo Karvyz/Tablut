dff9bdad66ba62fffbca2f306e43eb3bfa43dbec
4ppolyon <romain.alves@etu.univ-grenoble-alpes.fr>
2023-05-10 02:13:14 +0200

    add expert mais probleme dans les stats (voir gg doc)

--- Tablut.java
+++ Tablut.java
@@ -1,21 +1,18 @@
 
 import Controlleur.*;
 import Modele.Jeu;
-
-import Modele.TypeJoueur;
 import Modele.TypePion;
-import Vues.CollecteurEvenements;
-import Vues.InterfaceGraphique;
-
-import javax.swing.*;
 import java.util.ArrayList;
 
 
 public class Tablut{
     public static void main(String[] args) {
         ArrayList<IA> ias = new ArrayList<>();
-        ias.add(new IA_facile("", TypePion.ATTAQUANT, new Jeu()));
+        ias.add(new IA_difficile_Long_live_the_king("", TypePion.ATTAQUANT, new Jeu()));
+        ias.add(new IA_difficile_MassacrePion("", TypePion.ATTAQUANT, new Jeu()));
         ias.add(new IA_difficile_le_roi_c_ciao("", TypePion.ATTAQUANT, new Jeu()));
+        ias.add(new IA_difficile_AttaqueRoi("", TypePion.ATTAQUANT, new Jeu()));
+        ias.add(new IA_expert("", TypePion.ATTAQUANT, new Jeu()));
         TournoisControlleur tournoisControlleur = new TournoisControlleur(ias, 100);
         tournoisControlleur.tournois();
     }
