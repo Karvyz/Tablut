0106223128396618de658904275c8ae9ce8f49ce
Henri Gros <henrigros1@gmail.com>
2023-05-10 08:59:25 +0200

    Automerge

--- Tablut.java
+++ Tablut.java
@@ -13,11 +13,18 @@ import java.util.ArrayList;
 
 public class Tablut{
     public static void main(String[] args) {
-        ArrayList<IA> ias = new ArrayList<>();
-        ias.add(new IA_facile("", TypePion.ATTAQUANT, new Jeu()));
-        ias.add(new IA_difficile_le_roi_c_ciao("", TypePion.ATTAQUANT, new Jeu()));
-        TournoisControlleur tournoisControlleur = new TournoisControlleur(ias, 100);
-        tournoisControlleur.tournois();
+        if (args.length == 0) {
+            Jeu j = new Jeu();
+            CollecteurEvenements control = new ControlleurMediateur(j);
+            InterfaceGraphique.demarrer(control);
+        }
+        else {
+            ArrayList<IA> ias = new ArrayList<>();
+            ias.add(new IA_facile("", TypePion.ATTAQUANT, new Jeu()));
+            ias.add(new IA_difficile_le_roi_c_ciao("", TypePion.ATTAQUANT, new Jeu()));
+            TournoisControlleur tournoisControlleur = new TournoisControlleur(ias, 100);
+            tournoisControlleur.tournois();
+        }
     }
 }
  
