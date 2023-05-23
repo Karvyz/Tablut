5b3b6fd50babaf3008023b47d534f454b5cb01e3
Henri Gros <henrigros1@gmail.com>
2023-05-23 12:51:06 +0200

    Etages IAs

--- Modele/JoueursCreation.java
+++ Modele/JoueursCreation.java
@@ -12,9 +12,9 @@ public class JoueursCreation {
             case HUMAIN:
                 return new Controlleur.Humain(nom, roleJ, jeu);
             case IA_FACILE:
-                return new Controlleur.IA_facile(nom, roleJ, jeu);
-            case IA_MOYEN:
                 return new IA_MonoEtage(nom, roleJ, jeu, new HeuristiqueMonteCarlo(100));
+            case IA_MOYEN:
+                return new Controlleur.IA_DifficileProfondeur(nom ,roleJ, jeu, new HeuristiqueFusion(0.4668334F, 0.33374965F,0.9967921F, 0.5499482F), 3);
             case IA_DIFFICILE:
                 return new Controlleur.IA_DifficileProfondeur(nom ,roleJ, jeu, new HeuristiqueFusion(0.4668334F, 0.33374965F,0.9967921F, 0.5499482F), 4);
         }
